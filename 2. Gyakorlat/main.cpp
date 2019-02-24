60c6453de461b6d18ced470c396a881a155ada05
attila gyen <gyenattila@gmail.com>
2019-02-24 15:50:02 +0100

    gyakorlaton átnézett mintakód + friend függvén

--- 2. Gyakorlat/main.cpp
+++ 2. Gyakorlat/main.cpp
@@ -61,7 +61,7 @@ Player::Player(const std::string& name, int positionX = 0, int positionY = 0)
 }
 */
 
-// paraméteres konstruktor member initialization list-el
+// paraméteres konstruktor member initializer list-el
 /*
 Player::Player(const std::string& name_, int positionX_ = 0, int positionY_ = 0)
 	: name(name_), positionX(positionX_), positionY(positionY_) { }
@@ -80,52 +80,52 @@ const std::string& Player::getPlayerName() const { return name; }
 
 int main(int argc, char const *argv[])
 {
-	// Player objektumpéldányok létrehozása
-	Player p1; // default konstruktor hívása
-	Player p2("Ryan"); // paraméteres konstruktor hívása az X és Y koordináták 0 értékkel
-	Player p3("Jack", 11, 2); // paraméteres konstruktor hívása az X = 11 és Y = 2 értékekkel
+    // Player objektumpéldányok létrehozása
+    Player p1; // default konstruktor hívása
+    Player p2("Ryan"); // paraméteres konstruktor hívása az X és Y koordináták 0 értékkel
+    Player p3("Jack", 11, 2); // paraméteres konstruktor hívása az X = 11 és Y = 2 értékekkel
 
-	/*
-	std::vector<Player> playersVector; // vektor létrehozása amiben Player objektumpéldányokat tudunk tárolni
-	
-	playersVector.push_back(p1);
-	playersVector.push_back(p2);
-	playersVector.push_back(p3);
+    /*
+    std::vector<Player> playersVector; // vektor létrehozása amiben Player objektumpéldányokat tudunk tárolni
 
-	// ha így adjuk át akkor minden egyes Player-ből egy másolat fog készülni
+    playersVector.push_back(p1);
+    playersVector.push_back(p2);
+    playersVector.push_back(p3);
+
+    // ha így adjuk át akkor minden egyes Player-ből egy másolat fog készülni
     // ami bekerül a vektorba, így ha szeretnénk módosítani pl. a p1-nek X és Y
     // koordinátáit (p1.setPlayerPosition(9,6);) az a vektoron belüli p1 objektumpéldányra
     // nem fog vonatkozni, mivel az csak egy másolata lesz a p1-nek. annak módosításához a
     // vektoron belüli indexével kell hivatkoznunk rá. (playersVector[0].setPlayerPosition(9,6);)
-    // egy jobb megoldás lenne, ha Player-re mutató pointereket tárolnánk és a Player-eknek a 
+    // egy jobb megoldás lenne, ha Player-re mutató pointereket tárolnánk és a Player-eknek a
     // referenciáját adnánk át
     */
 
     std::vector<Player*> playersVector; // vektor létrehozása amiben Player-re mutató pointerek fogunk tárolni
 	
-	// az egyes instance-ok referenciájának átadása
-	playersVector.push_back(&p1);
-	playersVector.push_back(&p2);
-	playersVector.push_back(&p3);
+    // az egyes instance-ok referenciájának átadása
+    playersVector.push_back(&p1);
+    playersVector.push_back(&p2);
+    playersVector.push_back(&p3);
 
-	// kiírásnál alőször dereferálnunk kell a mutatott objektumot majd ezek után a . operátorral segítségével tudunk
-	// metódushívást végrehajtani, azért kell zarójelezni, mert a . operátor erősebben köt, mint a *
-	for (int i = 0; i < playersVector.size(); ++i)
-		std::cout << "Player neve: " << (*playersVector[i]).getPlayerName() << ", X és Y pozíciója: " 
-			<< (*playersVector[i]).getXPosition() << ", " << (*playersVector[i]).getYPosition() << std::endl;
-	
-	p1.setPlayerPosition(9,6); // most, ha az eredeti példányon módosítunk akkor a vektorban lévő is módosul
-							   // ugyanis nem egy másolata van a vektorunkban csak egy mutató az eredeti p1-re
+    // kiírásnál először dereferálnunk kell a mutatott objektumot majd ezek után a . operátorral segítségével tudunk
+    // metódushívást végrehajtani, azért kell zarójelezni, mert a . operátor erősebben köt, mint a *
+    for (int i = 0; i < playersVector.size(); ++i)
+        std::cout << "Player neve: " << (*playersVector[i]).getPlayerName() << ", X és Y pozíciója: "
+            << (*playersVector[i]).getXPosition() << ", " << (*playersVector[i]).getYPosition() << std::endl;
 
-	std::cout << std::endl;
+    p1.setPlayerPosition(9,6); // most, ha az eredeti példányon módosítunk akkor a vektorban lévő is módosul
+                               // ugyanis nem egy másolata van a vektorunkban csak egy mutató az eredeti p1-re
 
-	// a -> (nyíl) operátor segítségével kiválthatjuk a dereferálást és a pont operátort és közvetlen elérjük a metódusokat
-	for (int i = 0; i < playersVector.size(); ++i)
-		std::cout << "Player neve: " << playersVector[i]->getPlayerName() << ", X és Y pozíciója: " 
-			<< playersVector[i]->getXPosition() << ", " << playersVector[i]->getYPosition() << std::endl;
+    std::cout << std::endl;
 
-	std::cout << std::endl;
-	playerCollisionDetection(p1, p2);
+    // a -> (nyíl) operátor segítségével kiválthatjuk a dereferálást és a pont operátort és közvetlen elérjük a metódusokat
+    for (int i = 0; i < playersVector.size(); ++i)
+        std::cout << "Player neve: " << playersVector[i]->getPlayerName() << ", X és Y pozíciója: "
+            << playersVector[i]->getXPosition() << ", " << playersVector[i]->getYPosition() << std::endl;
 
-	return 0;
+    std::cout << std::endl;
+    playerCollisionDetection(p1, p2);
+
+    return 0;
 }
