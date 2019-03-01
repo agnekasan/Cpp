0d798fa884d149bfb7f19b34bee4d88d7361bdf3
attila gyen <gyenattila@gmail.com>
2019-03-01 20:21:58 +0100

    copy ctor, assignment operator

--- 3. Gyakorlat/C++/cCtorAsOp.cpp
+++ 3. Gyakorlat/C++/cCtorAsOp.cpp
@@ -8,12 +8,14 @@ public:
 	A() : num(new int(1)) { }
 	~A() { delete num; }
 
+    // copy ctor
 	A(const A& rhs)
 	{
 		num = new int;
 		*num = *rhs.num;
 	}
-
+    
+    // assingment operator
 	A& operator=(const A& rhs)
 	{
 		if(this != &rhs)
@@ -23,27 +25,6 @@ public:
 		}
 
 		return *this;
-	}
-
-	bool operator<(const A& rhs)
-	{
-		if(this == &rhs)
-			return true;
-
-		return *num < *rhs.num;
-
-	}
-
-	bool operator==(const A& rhs)
-	{
-		if(this == &rhs)
-			return true;
-		return *num == *rhs.num;
-	}
-
-	int operator+(const A& rhs)
-	{
-		return *num + *rhs.num;
 	}
 
 	int* getNumRef() { return num; }
@@ -61,8 +42,6 @@ int main(int argc, char const *argv[])
 
 	std::cout << a1.getNumRef() << std::endl;
 	std::cout << a2.getNumRef() << std::endl;
-	std::cout << (a1 < a2) << std::endl;
-	std::cout << (a1 == a2) << std::endl;
-	std::cout << (a1 + a2) << std::endl;
+	
 	return 0;
 }
